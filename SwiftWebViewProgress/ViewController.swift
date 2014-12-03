149e1b436b20604419a24f53a7a325e631297ab5
mokumoku <da1_1hara@me.com>
2014-12-03 23:31:24 +0900

    Initial Commit

---
+++ SwiftWebViewProgress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWebViewProgress
+//
+//  Created by Daichi Ichihara on 2014/12/03.
+//  Copyright (c) 2014年 MokuMokuCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
